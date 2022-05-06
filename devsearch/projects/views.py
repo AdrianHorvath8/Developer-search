37a6ff0e7a29dd0ec9e9624f2059733b63cf5f1f
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-05-06 11:20:55 +0200

    Creating database

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def home (request):
-    return render(request, "projects/home.html")
+    context = []
+    return render(request, "projects/home.html", context)

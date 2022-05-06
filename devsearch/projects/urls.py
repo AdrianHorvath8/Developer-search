540cfc7ecd515d7d9f1ac66d3fa3e1364880f4e9
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-05-06 15:51:42 +0200

    Create project

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name="home"),
+    path('project/<str:pk>/', views.project,name="project"),
+    path('create_project', views.create_project,name="create_project"),
 ]
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Project, Tag, Review
+from . forms import ProjectForm
 
 def home (request):
-    context = {}
+    projects = Project.objects.all()
+    context = {"projects":projects}
     return render(request, "projects/home.html", context)
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+    context = {"project":project}
+    return render(request, "projects/project.html", context)
+
+
+def create_project(request):
+    form = ProjectForm()
+    if request.method == "POST":
+
+        form = ProjectForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)

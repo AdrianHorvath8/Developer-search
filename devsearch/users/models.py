773210bcba3b7c3f41e8b9645a24c0584937301f
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-05-14 12:10:26 +0200

    adding user profile template

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
+    
+
 class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, 
     primary_key=True, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True,blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=300, null= True, blank=True)
     short_info = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
@@ -17,7 +21,23 @@ class Profile(models.Model):
     social_linkedin = models.CharField(max_length=200, blank=True, null=True)
     social_youtube = models.CharField(max_length=200, blank=True, null=True)
     social_website = models.CharField(max_length=200, blank=True, null=True)
+    social_stackoverflow = models.CharField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Skill(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, 
+    primary_key=True, editable=False)
+    skill = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return str(self.name)
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,6 +1,16 @@
+
 from django.shortcuts import render
+from .models import Profile
 
 
 def profiles (request):
-    context = {}
+    profiles= Profile.objects.all()
+    context = {"profiles":profiles}
     return render(request, "users/profiles.html", context)
+
+def profile (request, pk):
+    profile= Profile.objects.get(id=pk)
+    skills_with_description = profile.skill_set.exclude(description__exact="")
+    skills_no_description = profile.skill_set.filter(description="")
+    context = {"profile":profile, "skills_no_description":skills_no_description, "skills_with_description":skills_with_description}
+    return render(request, "users/user_profile.html", context)

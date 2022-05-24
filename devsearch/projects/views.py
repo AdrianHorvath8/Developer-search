a927e2969f6fe1cf227b6714d9b6fd097e94ef13
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-05-24 10:43:51 +0200

    Adding profiles pagination

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,7 +4,6 @@ from . forms import ProjectForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
-
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def projects (request):
@@ -34,9 +33,19 @@ def projects (request):
         page = paginator.num_pages
         projects = paginator.page(page)
 
-    # video 16:55 paginatio
+    left_index = (int(page) - 4)
 
-    context = {"projects":projects, "search_query":search_query, "paginator":paginator}
+    if left_index <1:
+        left_index = 1
+
+    right_index = (int(page) + 5)
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages+1
+
+    custom_range= range(left_index,right_index)
+
+    context = {"projects":projects, "search_query":search_query, "paginator":paginator, "custom_range":custom_range}
     return render(request, "projects/projects.html", context)
 
 
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -71,10 +71,33 @@ def profiles (request):
     Q(skill__in = skill)
      )
 
+    page = request.GET.get("page")
+    paginator= Paginator(profiles, 6)
+
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        profiles = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        profiles = paginator.page(page)
+
+    left_index = (int(page) - 4)
+
+    if left_index <1:
+        left_index = 1
+
+    right_index = (int(page) + 5)
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages+1
+
+    custom_range= range(left_index,right_index)
     
 
 
-    context = {"profiles":profiles, "search_query":search_query}
+    context = {"profiles":profiles, "search_query":search_query,"custom_range":custom_range}
     return render(request, "users/profiles.html", context)
 
 

8e86a6961f586466b3e0049ec0ce3d9d851d06fb
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-06-11 11:20:54 +0200

    Trying to solve server error(500)

--- projects/views.py
+++ projects/views.py
@@ -21,32 +21,9 @@ def projects (request):
     Q(owner__name__icontains = search_query) 
        )
 
-    page = request.GET.get("page")
-    paginator= Paginator(projects, 6)
-
-    try:
-        projects = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1
-        projects = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        projects = paginator.page(page)
-
-    left_index = (int(page) - 4)
-
-    if left_index <1:
-        left_index = 1
-
-    right_index = (int(page) + 5)
-
-    if right_index > paginator.num_pages:
-        right_index = paginator.num_pages+1
     
 
-    custom_range= range(left_index,right_index)
-
-    context = {"projects":projects, "search_query":search_query, "paginator":paginator, "custom_range":custom_range}
+    context = {"projects":projects, "search_query":search_query,}
     return render(request, "projects/projects.html", context)
 
 

4f30c57617236925313f2b1728f7ce35ff5b5196
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-06-20 15:51:58 +0200

    Trying solve server error(500)

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     "corsheaders",
     "storages",
+    "whitenoise.runserver_nostatic",
 ]
 
 
@@ -104,6 +106,7 @@ MIDDLEWARE = [
 
     
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'devsearch.urls'
@@ -142,6 +145,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -197,6 +203,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

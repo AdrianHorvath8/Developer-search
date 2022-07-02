cf7c12ec1166ed3c39a553c7ab33ea12967309f7
Adrian Horvath <adrian.horvath.ah8@gmail.com>
2022-07-02 12:18:33 +0200

    Trying to solve server error(500)

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -206,7 +206,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 
 AWS_QUERYSTRING_AUTH = False
@@ -215,9 +220,7 @@ AWS_S3_FILE_OVERWRITE  = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY =os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
 
 
 #if os.getcwd() == '/app':
